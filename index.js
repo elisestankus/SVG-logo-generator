458d6f63e2615387fc904eff220b2fb46e722955
estan <elise.stankus@gmail.com>
2023-11-29 06:51:53 -0800

    added questions array for inquirer

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateLogo = require('./lib/generateLogo')
 
+const questions = [
+    {
+        type: 'input',
+        name: 'initials',
+        message: 'Please enter your initials',
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What color would you like your logo to be?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like your logo to be?',
+        choices: ['Circle', 'Triangle', 'Square']
+    }
+]
